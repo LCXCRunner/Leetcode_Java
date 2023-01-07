d5758ca1285ff19af8310df2e4fa2814c58aaf4e
Jake Gillow <jake.gillow@gmail.com>
2023-01-06 21:43:05 -0700

    copied in code

--- 13.roman_numeral_to_integer/src/App.java
+++ 13.roman_numeral_to_integer/src/App.java
@@ -1,5 +1,47 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String s = "";
+
+        s = "LIV";
+        System.out.println(romanToInt(s));
+
     }
+
+public static int romanToInt(String s) {
+    int output = 0;
+    int length = s.length();
+    char tempVar = 'a';
+
+    for (int i = length; i > 0; i--){
+        tempVar = s.charAt(i-1);
+        if(tempVar == 'I'){
+            output = output + 1;
+        }
+        if(tempVar == 'V'){
+            output = output + 5;
+        }
+        if(tempVar == 'X'){
+            output = output + 10;
+        }
+        if(tempVar == 'L'){
+            output = output + 50;
+        }
+        if(tempVar == 'C'){
+            output = output + 100;
+        }
+        if(tempVar == 'D'){
+            output = output + 500;
+        }
+        if(tempVar == 'M'){
+            output = output + 1000;
+        }
+        else{
+            continue;
+        }
+        
+    }
+
+    return output;
 }
+}
+
