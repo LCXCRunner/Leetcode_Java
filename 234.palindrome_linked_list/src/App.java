4ad521045f8d2481e0a201e643530831b8d30090
Jake Gillow <jake.gillow@gmail.com>
2023-01-07 18:15:40 -0700

    moving to main computer.

--- 234.palindrome_linked_list/src/App.java
+++ 234.palindrome_linked_list/src/App.java
@@ -1,5 +1,20 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+import java.util.LinkedList;
+
+public class App { 
+  public static void main(String[] args) { 
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    list.add(1);
+    list.add(1);
+    list.add(8);
+    list.add(9);
+    System.out.println(list);
+    isPalindrome(list);
+  } 
+
+public static boolean isPalindrome(LinkedList<Integer> intList){
+    int halfListLength = intList.size() / 2;
+
+    
+    return false;
+}
 }
