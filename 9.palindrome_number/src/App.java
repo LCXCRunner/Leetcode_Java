2efa67081d60f6e028d15a419805e5d75e08e3f2
LCXCRunner <jake.gillow@gmail.com>
2023-03-01 16:50:53 -0700

    started and outlined method

---
+++ 9.palindrome_number/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(isPalindrome(121));
+    }
+    public static boolean isPalindrome(int x){
+        if(x < 0){
+            return false;
+        }
+        String xString = Integer.toString(x);
+        int i = 0;
+        int j = xString.length() - 1;
+
+        while(i != j || i < j){
+            if(xString.charAt(i) == xString.charAt(j)){
+                continue;
+            }
+            else{
+                return false;
+            }
+
+        }
+        return false;
+    }
+}
