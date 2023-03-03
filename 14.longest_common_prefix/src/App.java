a6ef97924181b0669e917e2a232fc1efa2b029a7
LCXCRunner <jake.gillow@gmail.com>
2023-03-03 11:59:41 -0700

    updated for new test cases - solved

--- 14.longest_common_prefix/src/App.java
+++ 14.longest_common_prefix/src/App.java
@@ -2,21 +2,24 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String[] tester = {"dog","racecar","car"};
+        String[] tester = {""};
         System.out.println(longestCommonPrefix(tester));
 
     }
 
     public static String longestCommonPrefix(String[] strs){
-        String result = "";
-        Arrays.sort(strs); // sort in alphabetical order, meaning the shortest will be first and longest last
+        Arrays.sort(strs);
         String first = strs[0];
-        String last = strs[strs.length - 1];
-        int i = 1;
-        while(first.substring(0, i).equals(last.substring(0, i))){
-            result = first.substring(0, i);
-            i++;
+        String last = strs[strs.length-1];
+        int idx = 0;
+        while(idx < first.length() && idx < last.length()){
+            if(first.charAt(idx) == last.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
         }
-        return result;
+        return first.substring(0, idx);
+        }
     }
-}
+
