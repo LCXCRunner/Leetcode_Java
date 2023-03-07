c2e33101f94c82ea191ef2ba14957afe42c5a9ee
LCXCRunner <jake.gillow@gmail.com>
2023-03-06 19:23:33 -0700

    Adjusted approach. Solved

--- 234.palindrome_linked_list/src/App.java
+++ 234.palindrome_linked_list/src/App.java
@@ -1,33 +1,43 @@
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class App { 
   public static void main(String[] args) { 
-    LinkedList<Integer> list = new LinkedList<Integer>();
-    list.add(1);
-    list.add(2);
-    list.add(2);
-    list.add(1);
-    System.out.println(list);
-    System.out.println(isPalindrome(list));
+    ListNode head = new ListNode();
+    ListNode node2 = new ListNode(3, null);
+    ListNode node3 = new ListNode(2, null);
+    ListNode node4 = new ListNode(1, null);
+    head.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    head.val = 1; 
+    
+    System.out.println(isPalindrome(head));
   } 
 
-public static boolean isPalindrome(LinkedList<Integer> intList){
-    int Length = intList.size();
-    LinkedList<Integer> checkerList = new LinkedList<>();
-
-    for(int i = Length-1; i >= 0; i--){
-        checkerList.add(intList.get(i));
-    }
-
-    System.out.println(intList);
-    System.out.println(checkerList);
-
-    if(intList.equals(checkerList)){
-      return true;
-    }
-    else{
+  public static boolean isPalindrome(ListNode head){
+    if(head == null){
       return false;
     }
 
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    ListNode curr = head;
+
+    while(curr != null){
+      list.add(curr.val);
+      //System.out.println(curr.val);
+      curr = curr.next;
+    }
+
+    int length = list.size();
+    ArrayList<Integer> checker = new ArrayList<>();
+
+    for(int i = length - 1; i >= 0; i--){
+      checker.add(list.get(i));
+    }
+    if(checker.equals(list)){
+      return true;
+    }
+
+    return false;
 }
 }
