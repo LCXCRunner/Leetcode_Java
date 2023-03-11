15d945d82668d445213b6081e8b18111e1b94c68
LCXCRunner <jake.gillow@gmail.com>
2023-03-11 15:49:25 -0700

    solved for all test cases

--- 28.find_the_index_of_the_first_occurance_in_a_string/src/App.java
+++ 28.find_the_index_of_the_first_occurance_in_a_string/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String tester = "compoundword";
-        System.out.println(strStr(tester, "ruby"));
+        String tester = "abc";
+        System.out.println(strStr(tester, "c"));
     }
 
     public static int strStr(String haystack, String needle){
@@ -10,6 +10,12 @@ public class App {
                 
         for(int i = 0; i < haystackL; i++){
             //System.out.print(haystack.charAt(i) + ", ");
+            if(haystackL == 1 && haystack.substring(0).equals(needle)){
+                return 0;
+            }
+            if(haystackL < i + needleL){
+                break;
+            }
             if(haystack.substring(i, needleL + i).equals(needle)){
                 System.out.println(haystack.substring(i, needleL + i));
                 return i;
