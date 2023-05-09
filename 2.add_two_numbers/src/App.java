933ad5bda35d7c7925104dd33d9fc930db1dbd6c
LCXCRunner <jake.gillow@gmail.com>
2023-05-08 22:18:26 -0600

    adjusted approach, now works for huge numbers

--- 2.add_two_numbers/src/App.java
+++ 2.add_two_numbers/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Long tester1 = 9999999991L;
+        Long tester1 = 0L;
 
         ListNode head = longToLinkedListReverseOrder(tester1);
         ListNode tail = head;
@@ -13,7 +13,7 @@ public class App {
         System.out.println(tail.val);
 
 
-        Long tester2 = 9L;
+        Long tester2 = 100L;
 
         ListNode head2 = longToLinkedListReverseOrder(tester2);
         ListNode tail2 = head2;
@@ -24,51 +24,76 @@ public class App {
             tail2 = tail2.next;
         }
         System.out.println(tail2.val);
+
+        ListNode result = addTwoNumbers(head, head2);
+        ///*
+        ListNode tail3 = result;
+
+        System.out.print("Result: ");
+        while(tail3.next != null){
+            System.out.print(tail3.val + ", ");
+            tail3 = tail3.next;
+        }
+        System.out.println(tail3.val);
+        //*/
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
         ListNode head = new ListNode(0,null);
-        ListNode tail = new ListNode();
-        tail.next = head;
-        int list1 = l1.val;
-        int list2 = l2.val;
-        int multiplier = 10;
-        int resultInt = 0;
+        ListNode tail = new ListNode(0, head);
+        ListNode deadNode = new ListNode(0, null);
+        int carry = 0;
+        int remainder = 0;
+        int added = 0;
 
-        if(l1.val == 0 && l2.val == 0){
-            return head;
-        }
-
-        while(l1.next != null){
-            l1 = l1.next;
-            list1 = l1.val * multiplier + list1;
-            multiplier *= 10;
-        }
-        //System.out.println(list1);
-        multiplier = 10;
-
-        while(l2.next != null){
-            l2 = l2.next;
-            list2 = l2.val * multiplier + list2;
-            multiplier *= 10;
-        }
-        //System.out.println(list2);
-
-        resultInt = list1 + list2;
-        //System.out.println(resultInt);
-        
-        while(resultInt != 0){
-            ListNode newNode = new ListNode(resultInt % 10, null);
-            if(head.next == null){
-                head.next = newNode;
-                //head.val = newNode.val;
+        while(tail.next != null){
+            //find the value of the current place
+            ListNode newNode = new ListNode(0,null);
+            added = l1.val + l2.val + carry;
+            remainder = added % 10;
+            newNode.val = remainder;
+            if(added > 9){
+                carry = 1;
             }
             else{
-                tail.next = newNode;
+                carry = 0;
             }
-            tail = newNode;
-            resultInt /= 10;
+            
+            //add to result list and then move on to next node
+            if(head.next == null){
+                head.next = newNode;
+            }
+            else{
+                tail = tail.next;
+            }
+            tail.next = newNode;
+
+            //check if there are digits left in the list, if not then use zero
+            if(l1.next == null && l2.next == null){
+                if(carry == 1){//make sure to add the final floating carry, lists will be empty.
+                    ListNode finalNode = new ListNode(carry, null);
+                    tail = tail.next;
+                    tail.next = finalNode;
+                    return head.next;
+                }
+                else{
+                    return head.next;// stops if both source lists are empty
+                }
+            }
+            if(l1.next == null){
+                l1 = deadNode;
+            }
+            else{
+                l1 = l1.next;
+            }
+            if(l2.next == null){
+                l2 = deadNode;
+            }
+            else{
+                l2 = l2.next;
+            }
         }
+        
         return head.next;
     }
 
@@ -76,6 +101,11 @@ public class App {
     public static ListNode longToLinkedListReverseOrder(long num){
         ListNode head = new ListNode();
         ListNode tail = new ListNode();
+
+        if(num == 0){
+            head.val = 0;
+            return head;
+        }
 
         while(num != 0){
             ListNode newNode = new ListNode((int)(num % 10), null);
