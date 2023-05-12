ac5e4a2e24e2df555984f2780801efdb9e0d192d
LCXCRunner <jake.gillow@gmail.com>
2023-05-11 19:17:27 -0600

    starting a branch to pursue bit shifting

--- 29.divide_two_integers/src/App.java
+++ 29.divide_two_integers/src/App.java
@@ -1,10 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int dividend = Integer.MIN_VALUE;
-        int divisor = 2;
+        int divisor = -1109186033;
 
         System.out.println("Answer: " + (dividend/divisor));
-        System.out.print("Method Answer: " + divide(dividend,divisor));
+        System.out.println("Method Answer: " + divide(dividend,divisor));
+
+        //System.out.println(12<<0<<1);
     }
 
     public static int divide(int dividend, int divisor){
@@ -12,32 +14,39 @@ public class App {
         if(dividend == 0){
             return 0;
         }
-        if(dividend == Integer.MIN_VALUE && divisor == 1){//edge case
-            return Integer.MIN_VALUE;
+         if(dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;//edge case that is out of range of int
         }
-        if(dividend == Integer.MAX_VALUE && divisor == 1){//edge case
-            return Integer.MAX_VALUE;
-        }
-        if(dividend == Integer.MIN_VALUE && divisor == -1){//edge case
-            return Integer.MAX_VALUE;
-        }
-        if(dividend == Integer.MAX_VALUE && divisor == -1){//edge case
-            return Integer.MIN_VALUE;
-        }
-        if(dividend < 0 ^ divisor < 0){
+        if(dividend < 0 ^ divisor < 0){//XOR statement looking for negative answers
             isNegative = true;
         }
+        if(Math.abs(divisor) == 1){
+            int posDividend = Math.abs(dividend);
+            return (isNegative ? -posDividend : posDividend);
+        }
+        if(Math.abs(dividend>>1) < Math.abs(divisor>>1)){
+            return 0;
+        }
+       
 
-        long longDividend = Math.abs(dividend);
-        long longDivisor = Math.abs(divisor);
-        long runningSum = 0;
-        long count = 0;
+        int posDividend = Math.abs(dividend);
+        int posDivisor = Math.abs(divisor);
+        int runningSum = 0;
+        int count = 0;
 
-        while(runningSum <= longDividend){
-            runningSum += longDivisor;
+        if(dividend == Integer.MIN_VALUE){//edge case of Integer.MIN_VALUE
+            posDividend = Math.abs(dividend>>1);
+            while(posDividend - runningSum >= posDivisor){
+                runningSum += posDivisor;
+                count++;
+            }
+            count = count<<1;
+            return (isNegative ? -count : count);
+        }
+        while(posDividend - runningSum >= posDivisor){
+            runningSum += posDivisor;
             count++;
         }
-        count--;
-        return (int)(isNegative ? -count : count);
+        return (isNegative ? -count : count);
     }
 }
