5b6336e4b7858cf0a48bffc0039b7f58ebec1c85
LCXCRunner <jake.gillow@gmail.com>
2023-05-21 10:21:23 -0600

    added some comment sudo code

--- 43.multiply_strings/src/App.java
+++ 43.multiply_strings/src/App.java
@@ -3,14 +3,24 @@ public class App {
         String tester = "123456789";
         String tester2 = "987654321";
 
+        System.out.println(tester.substring(tester.length() - 1));
+
         System.out.println(multiply(tester, tester2));
     }
 
     public static String multiply(String num1, String num2){
-        long number1 = Long.parseLong(num1);
+
+        return "test";
+    }
+    /*
+     * long number1 = Long.parseLong(num1);
         long number2 = Long.parseLong(num2);
         Long result = number1 * number2;
 
         return result.toString();
-    }
+        I see two optoins
+        - multiply the final char in the string against each char in the first string and then sum all of the results
+        - brute for it, and just add the second number x amount of times to itself, x being number 1
+        - both have to be done using strings for length
+     */
 }
